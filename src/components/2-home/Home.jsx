761046644dcf6e802f7165e1ad568c7c3487221a
amirmejri96 <amirmejri1996@gmail.com>
2025-01-27 21:53:05 +0100

    links

--- src/components/2-home/Home.jsx
+++ src/components/2-home/Home.jsx
@@ -16,9 +16,9 @@ const Home = () => {
               me to consistently exceed client expectations while staying up-to-date with the latest industry trends and technologies.
         </p>
         <div className="all-icons flex">
-          <div className="icon icon-facebook"/>
-          <div className="icon icon-instagram"/>
-          <div className="icon icon-github"/>
+          <div><a href="https://www.facebook.com/profile.php?id=100007273906351" className="icon icon-facebook"></a></div>
+          <div><a href="https://www.instagram.com/Amir_mejri96/?fbclid=IwY2xjawIEzeRleHRuA2FlbQIxMAABHe2HeJ7SGS-0OoSQhBBmX2uODgUq7vIc6UvJNB9P3NmT1Wr2GUbXZnuKGA_aem_cDl8EVqngc797AX97Ii54Q#" className="icon icon-instagram"></a></div>
+          <div><a href="https://github.com/amirmejri96" className="icon icon-github"></a></div>
         </div>
 
       </div>
