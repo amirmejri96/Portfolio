96e90db8d2fb89c8f50ae8213623ba562cffb0ff
amirmejri96 <amirmejri1996@gmail.com>
2025-02-28 00:28:51 +0100

    add new project

--- src/components/4-projects/Projects.jsx
+++ src/components/4-projects/Projects.jsx
@@ -8,14 +8,14 @@ const Projects = () => {
         <div className="project-card">
           <img src="../../../shoesvap.png" alt="Projet 1"/>
             <h3>ShoesVap</h3>
-            <p>A website for a professional steam cleaning service.</p>
+            <p>A website of a professional steam cleaning service.</p>
             <a href="https://shoesvap.tn">Visit the website</a>
         </div>
 
         <div className="project-card">
           <img style={{width:'77%'}} src="../../../loconada.png" alt="Projet 2"/>
             <h3>Loconada</h3>
-            <p>A website for the party event Loconada.</p>
+            <p>A website of the party event Loconada.</p>
             <a href="#">Visit the website</a>
         </div>
 
@@ -23,6 +23,13 @@ const Projects = () => {
           <img style={{width:'80%'}} src="../../../maladie-coeliaque.png" alt="Projet 3"/>
             <h3>La maladie coelique</h3>
             <p>A website to describe the Celiac disease.</p>
+            <a href="#">Visit the website</a>
+        </div>
+
+        <div className="project-card">
+          <img style={{width:'80%'}} src="../../../cj studio.png" alt="Projet 3"/>
+            <h3>CJ Studio</h3>
+            <p>A website of an agency of photography and videography.</p>
             <a href="#">Visit the website</a>
         </div>
       </div>
